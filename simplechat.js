2389492a6d209346af3f451748eecd7a76968bec
Matthew Lavine <hermitlavine@gmail.com>
2015-01-10 00:44:00 -0500

    Fixed typo in name change, removed users list

--- simplechat.js
+++ simplechat.js
@@ -21,60 +21,52 @@ app.get('*', function (req, res) {
     res.redirect('/');
 });
 
-Array.prototype.remove = function() {
-    var what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
-    }
-    return this;
-};
-
-var users = [];
 var messages = [];
 
 io.on('connection', function (socket) {
     var username = 'Guest' + Math.floor(Math.random() * (10000 - 1 + 1)) + 1;
-    users.push(username);
+
     socket.emit('username', username);
-    for(var i = 0 ; i < messages.length ; i++) {
+
+    for (var i = 0; i < messages.length; i++) {
         socket.emit('message', messages[i]);
     }
+
     io.emit('message', {
         from: 'System',
         content: username + ' has joined chat.'
     });
+
     messages.push({
         from: 'System',
         content: username + ' has joined chat.'
     });
+
     socket.emit('message', {
         from: 'System',
         content: 'Hello ' + username + ', welcome to Simple-Chat!'
     });
 
-
     socket.on('username', function (newUsername) {
-        if(username == newUsername) {
+        if (username == newUsername) {
             return;
         }
-        users = users.remove(username);
+
         io.emit('message', {
             from: 'System',
-            content: username + ' is now know as ' + newUsername + '.'
+            content: username + ' is now known as ' + newUsername + '.'
         });
+
         messages.push({
             from: 'System',
-            content: username + ' is now know as ' + newUsername + '.'
+            content: username + ' is now known as ' + newUsername + '.'
         });
+
         username = newUsername;
-        users.push(username);
     });
 
     socket.on('message', function (message) {
-       io.emit('message', message);
+        io.emit('message', message);
         messages.push(message);
     });
 
@@ -83,6 +75,7 @@ io.on('connection', function (socket) {
             from: 'System',
             content: username + ' has left chat.'
         });
+
         messages.push({
             from: 'System',
             content: username + ' has left chat.'
